c9ea1428463bd903c33676bc49c7081eb7e1474f
Petr Sapak <petr.sapak@yahoo.com>
2021-06-12 11:27:56 +0200

    Add Team tests

--- TeamGenerator/TeamGenerator.Core.Test/GeneratorTest.cs
+++ TeamGenerator/TeamGenerator.Core.Test/GeneratorTest.cs
@@ -2,7 +2,7 @@
 
 namespace TeamGenerator.Core.Test
 {
-    class GeneratorTest : TestBase
+    class GeneratorTest
     {
 
     }
--- TeamGenerator/TeamGenerator.Core.Test/TestBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using NUnit.Framework;
-
-namespace TeamGenerator.Core.Test
-{
-    [TestFixture]
-    public class TestBase
-    {
-    }
-}
