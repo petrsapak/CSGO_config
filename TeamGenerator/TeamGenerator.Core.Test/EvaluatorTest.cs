cbf26d38fe29552ff9a6c467ddc17a29c5bb2a1c
Petr Sapak <petr.sapak@yahoo.com>
2021-06-13 18:05:47 +0200

    Add real example test and set the default evaluation

--- TeamGenerator/TeamGenerator.Core.Test/EvaluatorTest.cs
+++ TeamGenerator/TeamGenerator.Core.Test/EvaluatorTest.cs
@@ -10,32 +10,6 @@ namespace TeamGenerator.Core.Test
         private readonly IEvaluate evaluator = new BasicEvaluator();
 
         [Test]
-        [TestCase(Rank.Silver1, 1)]
-        [TestCase(Rank.Silver2, 3)]
-        [TestCase(Rank.Silver3, 5)]
-        [TestCase(Rank.Silver4, 7)]
-        [TestCase(Rank.SilverElite, 9)]
-        [TestCase(Rank.SilverEliteMaster, 11)]
-        [TestCase(Rank.GoldNova1, 14)]
-        [TestCase(Rank.GoldNova2, 17)]
-        [TestCase(Rank.GoldNova3, 20)]
-        [TestCase(Rank.GoldNovaMaster, 23)]
-        [TestCase(Rank.MasterGuardian1, 27)]
-        [TestCase(Rank.MasterGuardian2, 31)]
-        [TestCase(Rank.MasterGuardianElite, 35)]
-        [TestCase(Rank.DistinguishedMasterGuardian, 39)]
-        [TestCase(Rank.LegendaryEagle, 44)]
-        [TestCase(Rank.LegendaryEagleMaster, 49)]
-        [TestCase(Rank.SupremeMasterFirstClass, 54)]
-        [TestCase(Rank.GlobalElite, 64)]
-        public void EvaluatePlayer_ReturnsValueFromEnum(Rank rank, int expectedValue) 
-        {
-            Player player = new Player("", rank);
-
-            Assert.That(evaluator.EvaluatePlayer(player), Is.EqualTo(expectedValue));
-        }
-
-        [Test]
         public void EvaluateTeam_ReturnsSumOfindividualTeamMembersEvaluations()
         {
             Team team = EvaluatorTestHelper.GenerateRandomTeam();
